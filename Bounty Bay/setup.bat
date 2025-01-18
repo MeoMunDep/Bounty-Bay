379af648d31397d3fb4ebd77de42fafd6f9a073a
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-01-18 14:20:05 +0700

    fix error

--- Bounty Bay/setup.bat
+++ Bounty Bay/setup.bat
@@ -1,5 +1,5 @@
 @echo off
-title name Bot
+title Bounty Bay Bot by @MeoMunDep
 color 0A
 
 cd ..
@@ -14,7 +14,7 @@ if exist node_modules (
 :MENU
 cls
 echo =================================================================
-echo    name BOT SETUP AND RUN SCRIPT
+echo    Bounty Bay BOT SETUP AND RUN SCRIPT by @MeoMunDep
 echo =================================================================
 echo.
 echo Current directory: %CD%
@@ -97,7 +97,7 @@ if exist "..\node_modules" (
 ) else (
     echo Using node_modules from current directory
 )
-node bot
+node meomundep
 pause
 goto MENU
 
