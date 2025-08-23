31937e70b36644ddc1a7abe9fc119d45cbd17948
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-08-24 03:26:15 +0700

    update package

--- setup.sh
+++ setup.sh
@@ -67,7 +67,7 @@ while true; do
             clear
             print_yellow "Installing/Updating Node.js dependencies..."
             cd "$MODULES_DIR"
-            npm install user-agents axios colors p-limit https-proxy-agent socks-proxy-agent crypto-js ws uuid xlsx readline-sync
+            npm install user-agents axios meo-forkcy-proxy meo-forkcy-colors meo-forcky-utils
             cd - > /dev/null
             print_green "Dependencies installation completed!"
             read -p "Press Enter to continue..."
